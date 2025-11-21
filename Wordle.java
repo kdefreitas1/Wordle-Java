5efe5f19e9f4dc668d68b849790c9b61b5a0fa7e
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2025-11-21 09:44:03 -0500

    Added guess checker
    Display prints only grey and green does not work for yellow yet. Also does not stop guessing when word is correct.

--- Wordle.java
+++ Wordle.java
@@ -1,13 +1,45 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Wordle {
-    
+
+    public ArrayList<String> checkGuess(String guess, String answer) {
+        ArrayList<String> result = new ArrayList<String>();
+        
+        for (int i = 0; i < 5; i++) {
+            result.add("grey");
+        }
+        for (int i = 0; i < guess.length(); i++) {
+            if (guess.substring(i, i+1).equals(answer.substring(i, i+1))) {
+                result.set(i, "green");
+            }
+        }
+
+        return result;
+    }
+
     public void start() {
         String answer = "apple";
+        
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter your guess: ");
-        String guess = input.nextLine();
+        int numOfGuesses = 1;
+        while(true) {
+            if (numOfGuesses > 5) {
+                break;
+            }
+            System.out.print(numOfGuesses + ". Enter your guess: ");
+            String guess = input.nextLine();
+            if(guess.length() != 5) {
+                System.out.println("Please enter a 5 letter word.");
+                continue;
+            }
+            ArrayList<String>result = checkGuess(guess, answer);
+            for(String color : result) {
+                System.out.print(color + " ");
+            }
+            System.out.println();
+            numOfGuesses++;
+        }
 
         input.close();
     }
