e51a834928746880574cd3383cb04bf6f1466d54
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2025-11-25 09:19:10 -0500

    Basic wordle game works now
    Yellow and end game should work now. The random words needs to be added.

--- Wordle.java
+++ Wordle.java
@@ -5,13 +5,16 @@ public class Wordle {
 
     public ArrayList<String> checkGuess(String guess, String answer) {
         ArrayList<String> result = new ArrayList<String>();
-        
+
         for (int i = 0; i < 5; i++) {
             result.add("grey");
         }
+
         for (int i = 0; i < guess.length(); i++) {
             if (guess.substring(i, i+1).equals(answer.substring(i, i+1))) {
                 result.set(i, "green");
+            } else if (answer.contains(guess.substring(i, i+1))) {
+                result.set(i, "yellow");
             }
         }
 
@@ -20,24 +23,36 @@ public class Wordle {
 
     public void start() {
         String answer = "apple";
-        
         Scanner input = new Scanner(System.in);
         int numOfGuesses = 1;
-        while(true) {
+
+        while (true) {
             if (numOfGuesses > 5) {
+                System.out.println("You lose. The answer was " + answer + ".");
                 break;
             }
+
             System.out.print(numOfGuesses + ". Enter your guess: ");
             String guess = input.nextLine();
-            if(guess.length() != 5) {
+
+            if (guess.length() != 5) {
                 System.out.println("Please enter a 5 letter word.");
                 continue;
             }
+
             ArrayList<String>result = checkGuess(guess, answer);
-            for(String color : result) {
+            
+            for (String color : result) {
                 System.out.print(color + " ");
             }
+
             System.out.println();
+
+            if (guess.equals(answer)) {
+                System.out.println("Correct. Game over.");
+                break;
+            }
+
             numOfGuesses++;
         }
 
